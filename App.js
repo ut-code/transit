d2135b963a12bdfce45482dcc6bce63c3fca1004
sawa3030 <esawada2003@gmail.com>
2024-02-26 20:16:03 +0900

    minor change in app.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>これは駅構内専用の乗り換え案内アプリです</Text>
       <StatusBar style="auto" />
     </View>
   );
